1e3e5cd1c2a291bf16f60b05345c75012a3279d5
Hugo De Grossi <91972302+hugo-degrossi@users.noreply.github.com>
2022-06-22 11:08:38 +0200

    Development (#16) (#17)
    * 7: sign up
    
    * 9: Message Patterns et error handling
    
    * 9: Error handling
    
    * Added unit and e2e tests, after issue #6
    
    Unit tests on:
    	- auth controller
    	- auth service
    	- refresh tokens service
    	- user service
    	- user controller
    
    And e2e tests on auth.
    Added unit and e2e tests
    
    Unit tests on:
    	- auth controller
    	- auth service
    	- refresh tokens service
    	- user service
    	- user controller
    
    And e2e tests on auth.
    
    * 9: Adapt ms
    
    * FIXING (#15)
    
    * FIXING
    
    * update CI
    
    Co-authored-by: Hugo De Grossi <personal@hugo-degrossi.fr>
    
    Co-authored-by: Slash-12 <baptiste.moench@viacesi.fr>
    Co-authored-by: Slash-12 <40773870+Slash-12@users.noreply.github.com>
    Co-authored-by: Stéphane Zundel <stephanezundel@Stephanes-MacBook-Pro.local>
    Co-authored-by: Zundel Stéphane <zundel.stephane@sfr.fr>
    Co-authored-by: Hugo De Grossi <personal@hugo-degrossi.fr>
    
    Co-authored-by: Slash-12 <baptiste.moench@viacesi.fr>
    Co-authored-by: Slash-12 <40773870+Slash-12@users.noreply.github.com>
    Co-authored-by: Stéphane Zundel <stephanezundel@Stephanes-MacBook-Pro.local>
    Co-authored-by: Zundel Stéphane <zundel.stephane@sfr.fr>
    Co-authored-by: Hugo De Grossi <personal@hugo-degrossi.fr>

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import {Injectable} from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm';
 import {UserEntity} from "./user.entity";
 import {Repository} from "typeorm";
+import {ICreateUserDto} from "./user.dto";
+import {RolesTypeEnum} from "../_enums/roles-type.enum";
+
 
 @Injectable()
 export class UserService {
-
     constructor(@InjectRepository(UserEntity) private _repository: Repository<UserEntity>) {}
+
+    async find(): Promise<UserEntity[]> {
+        return await this._repository.find();
+    }
 
     async findOne(id: number): Promise<UserEntity> {
         return await this._repository.findOne({ where: { id }});
@@ -16,4 +22,43 @@ export class UserService {
         return await this._repository.findOne({ where: { email }});
     }
 
+    async create(user: ICreateUserDto): Promise<UserEntity> {
+        const {confirmPassword, ...rest} = user;
+        const createdUser = await this._repository.create(rest);
+        createdUser.role = RolesTypeEnum.USER;
+        createdUser.createdAt = new Date();
+        return await this._repository.save(createdUser)
+    }
+
+    hasPasswordNumber(password: string): boolean {
+        return (new RegExp(/.*[0-9]/)).test(password);
+    }
+
+    hasPasswordSpecialChar(password: string): boolean {
+        return (new RegExp(/.*[!@#$%^&*]/)).test(password);
+    }
+
+    hasPasswordUpper(password: string): boolean {
+        return (new RegExp(/.*[A-Z]/)).test(password);
+    }
+
+    hasPasswordLower(password: string): boolean {
+        return (new RegExp(/.*[a-z]/)).test(password);
+    }
+
+    isLongEnough(password: string): boolean {
+        return (new RegExp(/.{8,}/)).test(password);
+    }
+
+    isEmailValid(email: string): boolean {
+        return (new RegExp(/^[a-z0-9_]+\.[a-z0-9_]+\@[a-z0-9_]+\.[a-z]{2,4}$/)).test(email);
+    }
+
+    isPasswordValid(password: string): boolean {
+        return this.hasPasswordNumber(password)
+            && this.hasPasswordSpecialChar(password)
+            && this.hasPasswordUpper(password)
+            && this.hasPasswordLower(password)
+            && this.isLongEnough(password)
+    }
 }
