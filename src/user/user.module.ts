20a572b7505221ec085a934a7d45805211b7e793
Hugo De Grossi <91972302+hugo-degrossi@users.noreply.github.com>
2022-06-22 11:03:16 +0200

    Development (#16)
    * 7: sign up
    
    * 9: Message Patterns et error handling
    
    * 9: Error handling
    
    * Added unit and e2e tests, after issue #6
    
    Unit tests on:
    	- auth controller
    	- auth service
    	- refresh tokens service
    	- user service
    	- user controller
    
    And e2e tests on auth.
    Added unit and e2e tests
    
    Unit tests on:
    	- auth controller
    	- auth service
    	- refresh tokens service
    	- user service
    	- user controller
    
    And e2e tests on auth.
    
    * 9: Adapt ms
    
    * FIXING (#15)
    
    * FIXING
    
    * update CI
    
    Co-authored-by: Hugo De Grossi <personal@hugo-degrossi.fr>
    
    Co-authored-by: Slash-12 <baptiste.moench@viacesi.fr>
    Co-authored-by: Slash-12 <40773870+Slash-12@users.noreply.github.com>
    Co-authored-by: Stéphane Zundel <stephanezundel@Stephanes-MacBook-Pro.local>
    Co-authored-by: Zundel Stéphane <zundel.stephane@sfr.fr>
    Co-authored-by: Hugo De Grossi <personal@hugo-degrossi.fr>

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,16 @@
-import {Module} from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import {UserController} from './user.controller';
 import {UserService} from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import {UserEntity} from "./user.entity";
+import {AuthModule} from "../auth/auth.module";
+import {RefreshTokensModule} from "../refresh-tokens/refresh-tokens.module";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([UserEntity]),
+        forwardRef(() => AuthModule),
+        RefreshTokensModule
     ],
     controllers: [UserController],
     providers: [UserService],
